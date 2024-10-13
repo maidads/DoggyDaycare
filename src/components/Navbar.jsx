cd38c6dd262c3fffaa160ce76a6af518710cd6c6
Maida <98819095+maidads@users.noreply.github.com>
2024-10-14 01:00:33 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {         // for all pages
     return (
         <nav className="navbar">
         <ul>
-            <li><Link to="/">Home</Link></li>
+            <li><Link to="/doggy-daycare">Home</Link></li>
             <li><Link to="/catalog">Catalog</Link></li>
             <li><Link to="/aboutdogs">About Dogs</Link></li>
         </ul>
