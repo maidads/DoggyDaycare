a5b0f42f0eeed76fc973fae3377b74002aea1d98
Maida <98819095+maidads@users.noreply.github.com>
2024-10-13 21:20:19 +0200

    adding blue welcome page with dog pic and description

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
-import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import './App.css';
-import AboutDogs from './components/AboutDogs';
-import CatalogPage from './components/CatalogPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import WelcomePage from './components/WelcomePage';
+import CatalogPage from './components/CatalogPage';
+import AboutDogs from './components/AboutDogs';
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/catalog">Catalog</Link></li>
-          <li><Link to="/aboutdogs">About Dogs</Link></li>
-        </ul>
-      </nav>
       <Routes>
         <Route path="/" element={<WelcomePage />} />
         <Route path="/catalog" element={<CatalogPage />} />
--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './WelcomePage.css';
 
 function WelcomePage() {
     return (
-        <div>
-            <h1>Welcome to Doggy Daycare!</h1>
+        <div className="welcome-container">
+        <nav className="navbar">
+            <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/catalog">Catalog</Link></li>
+            <li><Link to="/aboutdogs">About Dogs</Link></li>
+            </ul>
+        </nav>
+
+        <h1 className="welcome-title">Welcome to Doggy Daycare!</h1>
+        <img
+            src="https://s1.1zoom.me/big0/126/Dogs_Cute_Beautiful_Tongue_Retriever_Glance_Head_520706_1280x720.jpg"
+            alt="Cute Dog"
+            className="welcome-image"
+        />
+
+        <p className="welcome-description">
+            This is a dog daycare where we will take care of your dogs. We keep track of all our lovely dog guests so you can have peace of mind!
+        </p>
         </div>
     );
 }
