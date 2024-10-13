ecc7601daee8e7e60b8544a99cc4254a9f1de34a
Maida <98819095+maidads@users.noreply.github.com>
2024-10-13 20:13:32 +0200

    adding welcome page unedited

---
+++ src/WelcomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function WelcomePage() {
+    return (
+        <div>
+            <h1>Welcome to Doggy Daycare!</h1>
+        </div>
+    );
+}
+
+export default WelcomePage;
